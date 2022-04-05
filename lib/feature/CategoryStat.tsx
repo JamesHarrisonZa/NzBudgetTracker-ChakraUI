93f135ec1810adb5d5921938190f1a214c389c2b
James.Harrison <James.Harrison@Serko.com>
2022-04-06 09:20:18 +1200

    Refactor code to <CategoryStat />

---
+++ lib/feature/CategoryStat.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { StatLabel, Stat, StatNumber } from '@chakra-ui/react';
+import { useAccountTransactions } from '../data-access/useAccountTransactions';
+import { TransactionCategories } from '../util/transaction-categories';
+import { getFilteredTransactions } from '../util/get-filtered-transactions';
+import { getTransactionsTotal } from '../util/get-transactions-total';
+
+interface OwnProps {
+  label: string;
+  category: TransactionCategories;
+}
+
+export const CategoryStat: FC<OwnProps> = (props: OwnProps) => {
+  const { label, category } = props;
+
+  const { transactions, isLoading, isError } = useAccountTransactions();
+
+  const filteredTransactions = getFilteredTransactions(transactions, category);
+  const total = getTransactionsTotal(filteredTransactions);
+
+  return (
+    <Stat>
+      <StatLabel>{label}</StatLabel>
+      <StatNumber>${total}</StatNumber>
+    </Stat>
+  );
+};
--- lib/feature/Summary.tsx
+++ lib/feature/Summary.tsx
@@ -3,39 +3,16 @@ import {
   SimpleGrid,
   VStack,
   Text,
-  Box,
   StatLabel,
   Stat,
   StatNumber,
 } from '@chakra-ui/react';
 import FormattedDate from '../ui/FormattedDate';
-import { useAccountTransactions } from '../data-access/useAccountTransactions';
-import { getFilteredTransactions } from '../util/get-filtered-transactions';
-import { getTransactionsTotal } from '../util/get-transactions-total';
 import { TransactionCategories } from '../util/transaction-categories';
+import { CategoryStat } from './CategoryStat';
 
 export const Summary: FC = () => {
   const todayDate = new Date();
-
-  const { transactions, isLoading, isError } = useAccountTransactions();
-
-  const groceryTransactions = getFilteredTransactions(
-    transactions,
-    TransactionCategories.GroceryStoresAndSupermarkets
-  );
-  const groceriesTotal = getTransactionsTotal(groceryTransactions);
-
-  const fastFoodTransactions = getFilteredTransactions(
-    transactions,
-    TransactionCategories.FastFoodRestaurants
-  );
-  const fastFoodTotal = getTransactionsTotal(fastFoodTransactions);
-
-  const utilityTransactions = getFilteredTransactions(
-    transactions,
-    TransactionCategories.Utilities
-  );
-  const utilitiesTotal = getTransactionsTotal(utilityTransactions);
 
   return (
     <VStack w="full" h="full" p={10} spacing={10}>
@@ -44,20 +21,18 @@ export const Summary: FC = () => {
       </Text>
       <Text as="b">Amounts spent this month</Text>
       <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
-        <Stat>
-          <StatLabel>on groceries</StatLabel>
-          <StatNumber>${groceriesTotal}</StatNumber>
-        </Stat>
-
-        <Stat>
-          <StatLabel>on fast food</StatLabel>
-          <StatNumber>${fastFoodTotal}</StatNumber>
-        </Stat>
-
-        <Stat>
-          <StatLabel>on utilities</StatLabel>
-          <StatNumber>${utilitiesTotal}</StatNumber>
-        </Stat>
+        <CategoryStat
+          label="on groceries"
+          category={TransactionCategories.GroceryStoresAndSupermarkets}
+        />
+        <CategoryStat
+          label="on fast food"
+          category={TransactionCategories.FastFoodRestaurants}
+        />
+        <CategoryStat
+          label="on utilities"
+          category={TransactionCategories.Utilities}
+        />
       </SimpleGrid>
     </VStack>
   );
