7beae30a391d13d00e9a6e90414739a9ea2c19b8
James.Harrison <James.Harrison@Serko.com>
2022-04-06 09:40:15 +1200

    More broad categories. Round total to 2 decimal places

--- lib/feature/Summary.tsx
+++ lib/feature/Summary.tsx
@@ -1,15 +1,8 @@
 import { FC } from 'react';
-import {
-  SimpleGrid,
-  VStack,
-  Text,
-  StatLabel,
-  Stat,
-  StatNumber,
-} from '@chakra-ui/react';
+import { SimpleGrid, VStack, Text } from '@chakra-ui/react';
+import { CategoryStat } from './CategoryStat';
 import FormattedDate from '../ui/FormattedDate';
 import { TransactionCategories } from '../util/transaction-categories';
-import { CategoryStat } from './CategoryStat';
 
 export const Summary: FC = () => {
   const todayDate = new Date();
@@ -21,17 +14,30 @@ export const Summary: FC = () => {
       </Text>
       <Text as="b">Amounts spent this month</Text>
       <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
+        <CategoryStat label="on food" category={TransactionCategories.Food} />
         <CategoryStat
-          label="on groceries"
-          category={TransactionCategories.GroceryStoresAndSupermarkets}
-        />
-        <CategoryStat
-          label="on fast food"
-          category={TransactionCategories.FastFoodRestaurants}
+          label="on health"
+          category={TransactionCategories.Health}
         />
         <CategoryStat
           label="on utilities"
           category={TransactionCategories.Utilities}
+        />
+        <CategoryStat
+          label="on transport"
+          category={TransactionCategories.Transport}
+        />
+        <CategoryStat
+          label="on lifestyle"
+          category={TransactionCategories.Lifestyle}
+        />
+        <CategoryStat
+          label="on household"
+          category={TransactionCategories.Household}
+        />
+        <CategoryStat
+          label="on professionalFees"
+          category={TransactionCategories.ProfessionalFees}
         />
       </SimpleGrid>
     </VStack>
--- lib/util/get-transactions-total.ts
+++ lib/util/get-transactions-total.ts
@@ -1,4 +1,6 @@
 import { AccountTransactions } from '../data-access/accountTransaction';
 
-export const getTransactionsTotal = (transactions: AccountTransactions) =>
-  transactions.reduce((prev, curr) => prev + curr.amount, 0);
+export const getTransactionsTotal = (transactions: AccountTransactions) => {
+  const total = transactions.reduce((prev, curr) => prev + curr.amount, 0);
+  return total.toFixed(2);
+};
