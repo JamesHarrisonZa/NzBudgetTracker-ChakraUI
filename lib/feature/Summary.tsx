fec1809d290d97794a9fa9a8b5389987ecdb30b0
James.Harrison <James.Harrison@Serko.com>
2022-04-19 22:53:54 +1200

    Add FastFoodStat

--- lib/feature/Summary.tsx
+++ lib/feature/Summary.tsx
@@ -2,12 +2,13 @@ import { FC } from 'react';
 import { SimpleGrid, VStack, Text } from '@chakra-ui/react';
 import { TransactionType } from '../../pages/api/types/TransactionType';
 import { TransactionCategory } from '../../pages/api/types/TransactionCategory';
+import FormattedDate from '../ui/FormattedDate';
+import { MonthSelector } from './MonthSelector';
 import { TypeStat } from './stat/TypeStat';
 import { CategoryStat } from './stat/CategoryStat';
-import { MonthSelector } from './MonthSelector';
-import FormattedDate from '../ui/FormattedDate';
 import { GroceriesStat } from './stat/GroceriesStat';
 import { AlcoholStat } from './stat/AlcoholStat';
+import { FastFoodStat } from './stat/FastFoodStat';
 
 export const Summary: FC = () => {
   const todayDate = new Date();
@@ -19,7 +20,7 @@ export const Summary: FC = () => {
       </Text>
       <MonthSelector />
 
-      <SimpleGrid columns={{ sm: 1, md: 3, lg: 3 }} spacing={10}>
+      <SimpleGrid columns={{ sm: 2, md: 3, lg: 3 }} spacing={10}>
         <TypeStat label="Credit" type={TransactionType.Credit} />
         <TypeStat label="Debit Orders" type={TransactionType.StandingOrder} />
         <TypeStat label="Payments" type={TransactionType.Payment} />
@@ -28,13 +29,14 @@ export const Summary: FC = () => {
       <Text as="b" margin="0">
         Grouped categories
       </Text>
-      <SimpleGrid columns={{ sm: 1, md: 3, lg: 5 }} spacing={10}>
+      <SimpleGrid columns={{ sm: 2, md: 3 }} spacing={10}>
         <GroceriesStat />
         <AlcoholStat />
+        <FastFoodStat />
       </SimpleGrid>
 
       <Text as="b">High level categories</Text>
-      <SimpleGrid columns={{ sm: 1, md: 3, lg: 5 }} spacing={10}>
+      <SimpleGrid columns={{ sm: 2, md: 3, lg: 5, xl: 7 }} spacing={10}>
         <CategoryStat label="Food" category={TransactionCategory.Food} />
         <CategoryStat label="Health" category={TransactionCategory.Health} />
         <CategoryStat
