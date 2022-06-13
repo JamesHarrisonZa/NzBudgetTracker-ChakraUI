71411c52efec42c539ff89695002ada8a81dc21e
James.Harrison <James.Harrison@Serko.com>
2022-06-13 14:04:33 +1200

    Prefetch data for detail pages

--- pages/detail/alcohol.tsx
+++ pages/detail/alcohol.tsx
@@ -1,8 +1,23 @@
 import { FC } from 'react';
 import Head from 'next/head';
+import { dehydrate, QueryClient } from 'react-query';
 import Layout, { siteTitle } from '../../lib/ui/layout/Layout';
 import { TransactionCategory } from '../api/types/TransactionCategory';
 import { CategoriesDetail } from '../../lib/feature/detail/CategoriesDetail';
+import { prefetchAccountTransactions } from '../../lib/data-access/useAccountTransactions';
+
+export const getStaticProps = async () => {
+  const queryClient = new QueryClient();
+  await prefetchAccountTransactions(queryClient);
+
+  const tenMinutes = 10 * 60 * 1000;
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+    revalidate: tenMinutes,
+  }
+};
 
 const AlcoholDetail: FC = () => {
   const heading = 'Alcohol';
