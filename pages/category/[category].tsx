bbf40bf31ee7794b9c9577b437ce6a3282389367
James.Harrison <James.Harrison@Serko.com>
2022-06-13 14:15:38 +1200

    SSG/ISR for dynamic pages

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -1,9 +1,37 @@
 import { FC } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { dehydrate, QueryClient } from 'react-query';
 import Layout, { siteTitle } from '../../lib/ui/layout/Layout';
 import { CategoryDetail } from '../../lib/feature/detail/CategoryDetail';
 import { TransactionCategory } from '../api/types/TransactionCategory';
+import { prefetchAccountTransactions } from '../../lib/data-access/useAccountTransactions';
+
+export const getStaticProps = async () => {
+  const queryClient = new QueryClient();
+  await prefetchAccountTransactions(queryClient);
+
+  const tenMinutes = 10 * 60 * 1000;
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+    revalidate: tenMinutes,
+  }
+};
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { category: TransactionCategory.Utilities } },
+      { params: { category: TransactionCategory.Transport } },
+      { params: { category: TransactionCategory.Lifestyle } },
+      { params: { category: TransactionCategory.Household } },
+      { params: { category: TransactionCategory.ProfessionalFees } },
+    ],
+    fallback: true,
+  }
+}
 
 const Category: FC = () => {
   const router = useRouter();
--- pages/type/[type].tsx
+++ pages/type/[type].tsx
@@ -1,9 +1,35 @@
 import { FC } from 'react';
 import Head from 'next/head';
+import { dehydrate, QueryClient } from 'react-query';
 import { useRouter } from 'next/router';
 import { TransactionType } from '../api/types/TransactionType';
 import Layout, { siteTitle } from '../../lib/ui/layout/Layout';
 import { TypeDetail } from '../../lib/feature/detail/TypeDetail';
+import { prefetchAccountTransactions } from '../../lib/data-access/useAccountTransactions';
+
+export const getStaticProps = async () => {
+  const queryClient = new QueryClient();
+  await prefetchAccountTransactions(queryClient);
+
+  const tenMinutes = 10 * 60 * 1000;
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+    revalidate: tenMinutes,
+  }
+};
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { type: TransactionType.Credit } },
+      { params: { type: TransactionType.StandingOrder } },
+      { params: { type: TransactionType.Payment } },
+    ],
+    fallback: true,
+  }
+}
 
 const Detail: FC = () => {
   const router = useRouter();
